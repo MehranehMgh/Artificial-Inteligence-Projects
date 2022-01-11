76bf6ddb7823e8a0c36ff28c6113202122dc6872
MehranehMgh <50359278+MehranehMgh@users.noreply.github.com>
2022-01-11 10:52:30 +0330

    Add files via upload

---
+++ Project2/DFS/ai.py
@@ -0,0 +1,43 @@
+import random
+from collections import deque
+from copy import deepcopy
+
+
+class Agent:
+    def __init__(self, perceive_func=None, agent_id=None):
+        self.perceive_func = perceive_func
+        self.my_id = agent_id
+
+        ######### EDITABLE SECTION #########
+
+        self.predicted_actions = []
+
+        ######### END OF EDITABLE SECTION #########
+
+    def act(self):
+        sensor_data = self.perceive_func(self)
+
+        ######### EDITABLE SECTION #########
+
+        if self.predicted_actions==[]: self.predicted_actions=self.dfs(sensor_data['Current_Env'])
+        action=self.predicted_actions.pop()
+
+        ######### END OF EDITABLE SECTION #########
+
+        return action
+
+    ######### VV EDITABLE SECTION VV #########
+    def dfs(self, game):
+        actions = ['left','right','up','down']
+        q = deque()
+        q.append([game, []])
+        while len(q) > 0:
+            node = q.pop()
+            if node[0].goal_test():
+                return node[1]
+            if random.random() > 0.8:
+                random.shuffle(actions)
+            for action in actions:
+                child_game = node[0].create_copy()
+                if 'has died' not in child_game.take_action(action, self.my_id):
+                    q.append([child_game, [action] + node[1]])
